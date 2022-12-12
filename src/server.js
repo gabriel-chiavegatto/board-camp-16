86ae27dc864ee6b8a07845b1c5f9ee1081bf800a
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-12-12 13:12:37 -0300

    rename: index >> server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import routes from "routes.js";
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(routes);
+
+server.listen(4000, ()=>{console.log("Server ON")});
